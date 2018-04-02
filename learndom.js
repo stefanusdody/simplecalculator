233fec2b2d695d34959f9481a277152bf96ebc2d
stefanus dody <36668856+stefanusdody@users.noreply.github.com>
2018-04-02 14:55:18 +0700

    Create learndom.js

---
+++ learndom.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <title></title>
+  </head>
+  <body>
+   <input id="numberOne" type="text">
+   <input id="numberTwo" type="text">
+   <button onclick="addNumber()">(+)</button>
+   <button onclick="substractionNumber()">(-)</button>
+   <button onclick="divisionNumber()">(/)</button>
+   <button onclick="multiplicationNumber()">(*)</button>
+   <button onclick="modulationNumber()">(%)</button>
+   <div>
+
+   </div>
+   <input type="text" id="hasil2">
+
+
+
+
+  <script type="text/javascript" src="./learndom.js">
+
+  </script>
+  </body>
+</html>
